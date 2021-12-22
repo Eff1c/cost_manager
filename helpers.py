e8210aef6ddb4d968493c6573560134ec29116c8
eff1c <illymartynyk@gmail.com>
2021-12-22 17:16:37 +0200

    Fix parser (regex)

--- helpers.py
+++ helpers.py
@@ -8,3 +8,11 @@ def ceil_to_hundredth(number: float) -> float:
 
 def convert_str_to_date(string: str) -> datetime:
     return datetime.strptime(string, '%d.%m.%Y')
+
+
+def check_to_add_to_biggest_costs(biggest_costs, max_len, key, value, date):
+    items = biggest_costs.items()
+    prices = map(ceil_to_hundredth, map(float, biggest_costs.keys()))
+
+    if len(items) < max_len or value > prices:
+        biggest_costs[key] = value + f"({date})"
--- parser.py
+++ parser.py
@@ -3,16 +3,26 @@ from collections import OrderedDict
 
 
 def parse_notes(text: str) -> OrderedDict:
-    parsed_data = re.findall(r"(\d{2}.\d{2}.\d{4}\n(?:\d+ - [\w() ]+\n)+)", text)
+    parsed_data = re.findall(r"((?:\d+.\d+.\d{4})\n(?:\d+ - (?:.+)(?:\n|$))+)", text)
+
+    biggest_costs = {}
+    max_len_biggest_costs = 10
 
     data = OrderedDict()
     for item in parsed_data:
-        date = re.search(r"\d{2}.\d{2}.\d{4}", item).group(0)
-        arr_info = re.findall(r"(\d+) - (.+)(\n|$)", item)
-        data[date] = [
-            {
-                "price": info[0], "comment": info[1]
-            } for info in arr_info
-        ]
+        date = re.search(r"\d+.\d+.\d{4}", item).group(0)
+        arr_info = re.findall(r"(\d+) - (.+)(?:\n|$)", item)
+
+        costs = []
+        for info in arr_info:
+            costs.append(
+                {
+                    "price": info[0],
+                    "comment": info[1]
+                }
+            )
+
+
+        data[date] = costs
 
     return data
